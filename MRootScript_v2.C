0425ddf7bfac2d49ae6de73eced680df1a88b82d
Uzair <latif.uzair@gmail.com>
2018-03-12 23:58:05 -0500

    4th commit: <201 dist correction to <=200

--- MRootScript_v2.C
+++ MRootScript_v2.C
@@ -911,11 +911,11 @@ Double_t *getAngleCorrections(Int_t num, Int_t antco, Int_t D56co,Double_t test0
   }
   
   if(psns>0){
-    if(num>8 && fabs(antco-D56co)<201){
+    if(num>8 && fabs(antco-D56co)<=200){
       test0=test0+0.1*(pi/180)*psns;
       test1=test1+0.1*(pi/180)*psns;
     }  
-    if(num<9 && fabs(antco-D56co)<201){
+    if(num<9 && fabs(antco-D56co)<=200){
       test0=test0+0.5*(pi/180)*psns;
       test1=test1+0.5*(pi/180)*psns;
     }
@@ -930,11 +930,11 @@ Double_t *getAngleCorrections(Int_t num, Int_t antco, Int_t D56co,Double_t test0
   }
 
   if(psns>0 && solnum==2){
-    if(num>8 && fabs(antco-D56co)<201){
+    if(num>8 && fabs(antco-D56co)<=200){
       test0=test0+1*(pi/180)*psns;
       test1=test1+1*(pi/180)*psns;
     }
-    if(num<9 && fabs(antco-D56co)<201){
+    if(num<9 && fabs(antco-D56co)<=200){
       test0=test0+2*(pi/180)*psns;
       test1=test1+2*(pi/180)*psns;
     }
